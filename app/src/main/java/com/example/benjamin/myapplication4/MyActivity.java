4b1c1149c02fd98893b484bc6a1143101728898e
benjamin munoz <benjamin@ackila.com>
2014-10-05 21:25:46 -0700

    Add project  using android studio

---
+++ app/src/main/java/com/example/benjamin/myapplication4/MyActivity.java
@@ -0,0 +1,49 @@
+package com.example.benjamin.myapplication4;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.TextureView;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
+
+
+public class MyActivity extends Activity {
+
+    public EditText editText;
+    public TextView textView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_my);
+    }
+    public void onclick(View view) {
+        editText = (EditText) findViewById(R.id.editText);
+        textView = (TextView) findViewById(R.id.textView);
+        String s = editText.getText().toString();
+        textView.setText("Hola "+s);
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.my, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+        if (id == R.id.action_settings) {
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+}
